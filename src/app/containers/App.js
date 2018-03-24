91351f3b02a41cb284255dd89c88a201d990ed80
alegabo93 <alegabo93@hotmail.com>
2018-03-23 22:08:02 -0600

    comp: get profile data and add it to sidebar

--- src/app/containers/App.js
+++ src/app/containers/App.js
@@ -1,27 +1,44 @@
 // Third-party dependencies
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import { Grid, Row, Col } from "react-bootstrap";
 
 // Own actions/constants/components
-import PostsContainer from "./PostsContainer";
+import Sidebar from "Containers/Sidebar";
+import getUserData from "Actions/User";
 
-function App() {
-  return (
-    <Grid>
-      <Row>
-        <Col md={12}>
-          <div className="flexbox-container">
-            <div className="sidebar">
-              <p>hola mundo</p>
+class App extends Component {
+  static propTypes = {
+    getUsers: PropTypes.func.isRequired
+  };
+
+  componentWillMount() {
+    this.props.getUsers();
+  }
+
+  render() {
+    return (
+      <Grid>
+        <Row>
+          <Col md={12}>
+            <div className="flexbox-container">
+              <div className="sidebar">
+                <Col md={12}>
+                  <Sidebar />
+                </Col>
+              </div>
+              <div className="content">
+                <h1>Hola Mundo</h1>
+              </div>
             </div>
-            <div className="content">
-              <PostsContainer />
-            </div>
-          </div>
-        </Col>
-      </Row>
-    </Grid>
-  );
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
 }
 
-export default App;
+export default connect(null, {
+  getUsers: getUserData
+})(App);
---
+++ src/app/containers/Sidebar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Row, Col, Image } from "react-bootstrap";
+
+function Sidebar({ profile }) {
+  const { avatar_url: avatar, name } = profile;
+  return (
+    <Col md={12}>
+      <Row>
+        <Col md={12}>
+          <Image src={avatar} circle responsive />
+          <h3 className="text-center">{name}</h3>
+        </Col>
+      </Row>
+    </Col>
+  );
+}
+
+Sidebar.propTypes = {
+  profile: PropTypes.shape({
+    avatar_url: PropTypes.string
+  }).isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    profile: state.user.data ? state.user.data : {}
+  };
+}
+
+export default connect(mapStateToProps)(Sidebar);
