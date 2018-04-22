cb733ded05409ebd1fa39fe1c1db74765409a38d
alegabo93 <alegabo93@hotmail.com>
2018-04-22 00:23:58 -0500

    fix: add PersonalData and PresentationBlock components

--- src/app/containers/Sidebar.js
+++ src/app/containers/Sidebar.js
@@ -2,21 +2,31 @@
 import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
 // Own actions/constants/components
-import Avatar from "Components/Avatar";
+import PersonalData from "Components/PersonalData";
+import PresentationBlock from "Components/PresentationBlock";
 
-function Sidebar({ profile }) {
+function Sidebar({ profile, presentation }) {
   const { avatar_url: img, name, bio, login } = profile;
   return (
-    <Col md={12}>
-      <Avatar img={img} name={name} ocupation={bio} username={login} />
-    </Col>
+    <Row>
+      <Col md={12}>
+        <PersonalData img={img} name={name} occupation={bio} username={login} />
+      </Col>
+
+      <Col md={12}>
+        {presentation.map(item => (
+          <PresentationBlock key={item.title} {...item} />
+        ))}
+      </Col>
+    </Row>
   );
 }
 
 Sidebar.propTypes = {
+  presentation: PropTypes.arrayOf(PropTypes.object).isRequired,
   profile: PropTypes.shape({
     avatar_url: PropTypes.string,
     name: PropTypes.string,
@@ -27,7 +37,8 @@ Sidebar.propTypes = {
 
 function mapStateToProps(state) {
   return {
-    profile: state.user.data ? state.user.data : {}
+    profile: state.user.data ? state.user.data : {},
+    presentation: state.presentation.data ? state.presentation.data : {}
   };
 }
 
